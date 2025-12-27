4c4489953d2cdfcc05b3ec32bde17a1205747b44
Josh Luque <josh.luque@gmail.com>
2025-12-27 15:32:05 +0100

    Refactor build config and add CI/CD workflows

--- skeleton/buildSrc/src/main/kotlin/ProjectConfigLoader.kt
+++
@@ -1,9 +0,0 @@
-import java.util.Properties
-import java.io.File
-
-fun loadProjectConfig(root: File): Properties {
-    val props = Properties()
-    val cfg = File(root, "config/project-config.properties")
-    if (cfg.exists()) cfg.inputStream().use { props.load(it) }
-    return props
-}
